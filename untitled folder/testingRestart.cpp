c3e9b1e77d34d59e13c3f7eec29170d706662857
Erik Slikkerveer <erikslikk41@gmail.com>
2017-09-13 16:47:35 -0700

    added testingRestart (USE THIS)

---
+++ untitled folder/testingRestart.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+char choice, mainMenu(), ch;
+double addition1, addition2, subtraction1, subtraction2, multiplication1, multiplication2;
+void additionMenu(), subtractionMenu(), multiplicationMenu();
+
+int main() {
+	bool endProgram = true;
+	while (endProgram) {
+		choice = mainMenu();
+
+		switch (choice) {
+		case 'a':
+			additionMenu();
+			break;
+		case 'b':
+			subtractionMenu();
+			break;
+		case 'c':
+			multiplicationMenu();
+			break;
+		case 'q':
+			endProgram = false;
+			break;
+		}
+	}
+}
+
+char mainMenu() {
+	system("cls");
+	cout << "Welcome to the math menu" << endl << endl;
+	cout << "A. \t \t Addition \t \t (X+Y)" << endl << endl;
+	cout << "B. \t \t Subtraction \t \t (X-Y)" << endl << endl;
+	cout << "C. \t \t Multiplication \t (X*Y)" << endl << endl;
+	// cout << ""
+	cout << "Q. \t \t Quit the program \t \t " << endl << endl;
+	cin >> choice;
+
+	if (choice == 'a') {
+		cout << "You have chosen addition." << endl << endl;
+	}
+	else if (choice == 'b') {
+		cout << "You have chosen subtraction." << endl << endl;
+	}
+
+	return choice;
+}
+
+void subtractionMenu() {
+	system("cls");
+	cout << "Welcome to the subtraction menu" << endl << endl;
+	cout << "Please input an X and Y to subtract" << endl << endl;
+	cin >> subtraction1;
+	cin >> subtraction2;
+	double subtractionOutput = (float)subtraction1 - subtraction2;
+	cout << subtractionOutput << " is " << subtraction1 << " - " << subtraction2 << endl << endl;
+
+	cin.ignore();
+	cout << "PRESS RETURN/ENTER TO HEAD BACK TO MAIN MENU";
+	ch = cin.get();
+}
+
+void additionMenu() {
+	system("cls");
+	cout << "You have chosen addition. Please input two numbers to add: " << endl << endl;
+	cin >> addition1;
+	cin >> addition2;
+	double additionOutput = addition1 + addition2;
+	cout << additionOutput << " is " << addition1 << " + " << addition2 << endl << endl;
+
+	cin.ignore();
+	cout << "PRESS RETURN/ENTER TO HEAD BACK TO MAIN MENU";
+	ch = cin.get();
+}
+
+void multiplicationMenu() {
+	system("cls");
+	cout << "You have chosen multiplication. Please input two numbers to multiple: " << endl << endl;
+	cin >> multiplication1;
+	cin >> multiplication2;
+	double multiplicationOutput = multiplication1 * multiplication2;
+	cout << multiplicationOutput << " is " << multiplication1 << " times " << multiplication2 << endl << endl;
+
+	cin.ignore();
+	cout << "PRESS RETURN/ENTER TO HEAD BACK TO MAIN MENU";
+	ch = cin.get();
+}
+
